49f847c61b1f97846b2a1e2ac3aa2a4608f03c60
maebeale <maebeale@gmail.com>
2014-07-25 16:11:49 -0400

    carrierwave setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  mount_uploader :profile_pic, ProfilePicUploader
+
   scope :admin, -> { where(role: 'admin') }
   scope :student, -> { where(role: 'student') }
 
