8038135e2352e42ab84ddd000849029ef996f709
David Marshall <dzmarshall@gmail.com>
2014-07-27 12:01:37 -0400

    Adjust login/logout with devise

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,18 +2,29 @@ class SessionsController < Devise::SessionsController
   api :POST, "/users/sign_in", "Create a session"
   param_group :login_info, UsersController
   def create
-    resource = warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#failure")
-    sign_in_and_redirect(resource_name, resource)
+    user = warden.authenticate!(:scope => :user, :recall => "#{controller_path}#failure")
+    sign_in_and_redirect(:user, user)
   end
 
-  def sign_in_and_redirect(resource_or_scope, resource=nil)
+  def sign_in_and_redirect(resource_or_scope, user=nil)
     scope = Devise::Mapping.find_scope!(resource_or_scope)
-    resource ||= resource_or_scope
-    sign_in(scope, resource) unless warden.user(scope) == resource
-    return render :json => {:success => true}
+    user ||= resource_or_scope
+    sign_in(scope, user) unless warden.user(scope) == user
+    render :json => {:success => true}
   end
 
   def failure
-    return render :json => {:success => false, :errors => ["Login failed."]}
+    render :json => {:success => false, :errors => ["Login failed."]}
+  end
+
+  def destroy
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(:user))
+    respond_to_on_destroy if signed_out
+  end
+
+  private
+
+  def respond_to_on_destroy
+    render :json => {:log_out => true }
   end
 end
