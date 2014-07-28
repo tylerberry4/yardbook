6337c5d6e7348d82bee42bf8cc9ca29fb5991708
maebeale <maebeale@gmail.com>
2014-07-28 14:28:49 -0400

    change settings for carrierwave initializer & uploader file

--- app/uploaders/profile_pic_uploader.rb
+++ app/uploaders/profile_pic_uploader.rb
@@ -1,25 +1,24 @@
 class ProfilePicUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  # Choose what kind of storage to use for this uploader:
-  # storage :file
-  # storage :fog
-
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-  def extension_white_list
-    %w(jpeg jpg png )
+
+  process :resize_to_fit => [200, 300]
+
+  version :thumb do
+    process :resize_to_fit => [100, 150]
   end
 
-  process :resize_to_fit => [400, 600]
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
- # version :web do
-    version :thumb do
-      process :resize_to_fit => [32, 32]
-    end
-    # version :preview  { process :resize_to_fit => [128, 128] }
-    # version :full     { process :resize_to_fit => [1024, 768] }
- # end
-
-end
+  def default_url
+    size = case version_name
+             when :thumb then "100x150"
+             else "200x300"
+           end
+    "holder.js/#{size}/text:#{size}/social"
+  end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,24 +8,33 @@ CarrierWave.configure do |config|
       :aws_secret_access_key  => Rails.application.secrets.aws_secret_access_key
   }
 
+  if Rails.env.production?
+    config.storage = :fog
+  else
+    config.storage = :file
+  end
+
+  config.fog_directory = 'ironyardbook'
+end
+
   # if Rails.env.production?
   #   config.storage = :fog
   # else
   #   config.storage = :file
   # end
 
-  if Rails.env.test? || Rails.env.development?
-    config.storage = :file
-    #config.enable_processing = false
-    #config.root = "#{Rails.root}/tmp/uploads/#{DateTime.now.to_f}.#{rand(999)}.#{rand(999)}"
-  else
-    config.storage = :fog
-  end
-
-
-  config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
-
-  config.fog_directory = 'ironyardbook'
-  config.fog_public     = false
-  config.fog_attributes = {}
-end
+  # if Rails.env.test? || Rails.env.development?
+  #   config.storage = :file
+  #   #config.enable_processing = false
+  #   #config.root = "#{Rails.root}/tmp/uploads/#{DateTime.now.to_f}.#{rand(999)}.#{rand(999)}"
+  # else
+  #   config.storage = :fog
+  # end
+  #
+  #
+  # config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
+  #
+  # config.fog_directory = 'ironyardbook'
+  # config.fog_public     = false
+  # config.fog_attributes = {}
+# end
